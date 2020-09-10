79e0714add995aa9c39adb31509fbe1749a6b681
jabojohnigan <jabojohnigan93@yahoo.com>
2020-09-09 23:06:57 -0700

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from 'src/app/hero';
-import {HEROES} from 'src/app/mock-heroes'
-
+// import {HEROES} from 'src/app/mock-heroes'
+import {HeroService} from 'src/app/hero.service'
+import {MessagesService} from 'src/app/messages.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -16,16 +17,24 @@ export class HeroesComponent implements OnInit {
 //   hero: Hero = {
 //   id: 1,
 //   name: "Captain Falcon"}
-//   constructor() { }
+  constructor(private heroService: HeroService,
+              private messagesService: MessagesService) { }
 
-  heroes = HEROES
+  heroes : Hero[]
   selectedHero: Hero
   ngOnInit(): void {
+  this.getHeroes()
   }
 
   onSelect(clickedHero: Hero){
   this.selectedHero = clickedHero;
+  this.messagesService.add(`HeroesComponent: Selected hero id=${clickedHero.id}`);
   console.log(clickedHero)
+  }
+
+  getHeroes(): void{
+  this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes)
   }
 
 }
